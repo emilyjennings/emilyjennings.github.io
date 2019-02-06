d3b4633d23854a8ed53429a658e07b0bc3b67d45
github username <github email address>
2019-02-06 18:23:26 -0500

    added js faded scrolling

--- js/main.js
+++ js/main.js
@@ -161,6 +161,24 @@ var fadeIn = setInterval(function(){
     opacity: '1'
   }, 1000);
 
+
+  $(window).on("load",function() {
+    $(window).scroll(function() {
+      var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+      $(".floatblock").each(function() {
+        /* Check the location of each desired element */
+        var objectBottom = $(this).offset().top + $(this).outerHeight();
+
+        /* If the element is completely within bounds of the window, fade it in */
+        if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+          if ($(this).css("opacity")==0) {$(this).fadeTo(900,1);}
+        } else { //object goes out of view (scrolling up)
+          if ($(this).css("opacity")==1) {$(this).fadeTo(900,0);}
+        }
+      });
+    }).scroll(); //invoke scroll-handler on page-load
+  });
+
 /*
 
   var displayGallery = function(whichGallery){
