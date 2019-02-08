133bd03507b5e3b48fc397adeb41ef872717eaf6
github username <github email address>
2019-02-08 15:14:35 -0500

    changed front page content and added some fading to more elements in js

--- js/main.js
+++ js/main.js
@@ -165,15 +165,15 @@ var fadeIn = setInterval(function(){
   $(window).on("load",function() {
     $(window).scroll(function() {
       var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-      $(".floatblock").each(function() {
+      $(".floatblock, .twainblock, .Thankblock").each(function() {
         /* Check the location of each desired element */
         var objectBottom = $(this).offset().top + $(this).outerHeight();
 
         /* If the element is completely within bounds of the window, fade it in */
         if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-          if ($(this).css("opacity")==0) {$(this).fadeTo(900,1);}
+          if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
         } else { //object goes out of view (scrolling up)
-          if ($(this).css("opacity")==1) {$(this).fadeTo(900,0);}
+          if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
         }
       });
     }).scroll(); //invoke scroll-handler on page-load
